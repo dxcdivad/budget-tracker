bea316c8e26a253e32a2a14fff5fc9bc76c9668c
David Hong <dxcdivad@gmail.com>
2018-03-26 09:52:21 -0700

    fixed typo

--- src/js/components/ExpenseEntries/ExpenseEntries.jsx
+++ src/js/components/ExpenseEntries/ExpenseEntries.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  updateExpenseDescription,
+  updateExpenseAmount,
+  addExpense
+} from './expenseActions';
 
-export default class ExpenseEntries extends React.Component {
+export default class ExpenseEntries extends Component {
   constructor(props) {
     super(props);
+
+    this.handleDescriptionInput = this.handleDescriptionInput.bind(this);
+    this.handleAmountInput = this.handleAmountInput.bind(this);
+    this.handleAddExpense = this.handleAddExpense.bind(this);
+  }
+
+  handleDescriptionInput(event) {
+    const { dispatch } = this.props;
+    const { value } = event.target;
+    dispatch(updateExpenseDescription(value));
+  }
+
+  handleAmountInput(event) {
+    const { dispatch } = this.props;
+    const { value } = event.target;
+    dispatch(updateExpenseAmount(value));
+  }
+
+  handleAddExpense() {
+    const { description, amount, dispatch } = this.props;
+    dispatch(addExpense(description, amount));
   }
 
 
   render() {
+    const { description, amount, lineItems } = this.props;
     return (
       <div className='card border-danger mb-3'>
         <div className='card-header text-white bg-danger'>Expense Entries</div>
@@ -18,6 +45,8 @@ export default class ExpenseEntries extends React.Component {
                 type='text'
                 className='form-control'
                 id='expense-description'
+                value={ description }
+                onChange={ this.handleDescriptionInput }
               />
             </div>
             <div className='form-group'>
@@ -28,12 +57,15 @@ export default class ExpenseEntries extends React.Component {
                   type='text'
                   className='form-control'
                   id='expense-amount'
+                  value={ amount }
+                  onChange={ this.handleAmountInput }
                 />
               </div>
             </div>
             <button
               type='button'
               className='btn btn-danger col-12 mb-5'
+              onClick={ this.handleAddExpense }
             >+ Add Expense
             </button>
             <table className='table table-sm table-hover'>
@@ -44,15 +76,17 @@ export default class ExpenseEntries extends React.Component {
                 </tr>
               </thead>
               <tbody>
+                {lineItems.map(lineItem => (
                 <tr>
-                  <td>Rent</td>
-                  <td>$1,500.00</td>
+                  <td>{ lineItem.description }</td>
+                  <td>${ lineItem.amount.toFixed(2) }</td>
                 </tr>
+                  ))}
               </tbody>
             </table>
           </form>
         </div>
       </div>
-    )
+    );
   }
 }
--- src/js/components/ExpenseEntries/expenseReducer.js
+++ src/js/components/ExpenseEntries/expenseReducer.js
@@ -2,10 +2,37 @@ const defaultState = {
     description: '',
     amount: '',
     lineItems: []
-}
-
-export default function ExpenseReducer (state = defaultState, action) {
-
-
-    return state;
+  };
+  
+  export default function expenseReducer(state = defaultState, action) {
+    const { type, payload } = action;
+  
+    switch (type) {
+      case 'UPDATE_EXPENSE_DESCRIPTION': {
+        return {
+          ...state,
+          description: payload.description
+        };
+      }
+  
+      case 'UPDATE_EXPENSE_AMOUNT': {
+        return {
+          ...state,
+          amount: payload.amount
+        };
+      }
+  
+      case 'ADD_EXPENSE': {
+        const { description, amount } = action.payload;
+        return {
+          description: '',
+          amount: '',
+          lineItems: [...state.lineItems, { description, amount }]
+        };
+      }
+  
+      default: {
+        return state;
+      }
+    }
 }
