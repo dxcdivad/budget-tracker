0efaf4890b6e49c70056c5cb19b575f32557d6af
David Hong <dxcdivad@gmail.com>
2018-03-26 10:22:40 -0700

    updated test

--- test/hello.spec.js
+++ test/hello.spec.js
@@ -6,6 +6,8 @@ import App from './../src/js/app';
 
 describe('App Component', () => {
   it('should shallow render', () => {
-    expect(shallow(<App />).contains(<div className='hello'>Hello World</div>)).to.equal(true);
+    expect(shallow(<App />).contains(<div className='jumbotron' >
+    <h1 className='display-3 text-center'>Budget Tracker</h1>
+  </div>)).to.equal(true);
   });
 });
